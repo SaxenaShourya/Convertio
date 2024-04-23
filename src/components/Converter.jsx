31ed82d172d9be8a9b5ccb88cded54950b9a8462
Shourya Saxena <saxenashourya000@gmail.com>
2024-04-23 18:34:41 +0530

    feat(data): Populate currency dropdown with dynamic data from API

--- src/components/Converter.jsx
+++ src/components/Converter.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Card,
   CardHeader,
@@ -7,16 +7,42 @@ import {
   Button,
   Input,
 } from "@nextui-org/react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 import CurrencyDropdown from "./CurrencyDropdown";
 
 const Convertor = () => {
+  // API route for fetching all currencies: api.frankfurter.app/currencies
+
+  const [currencies, setCurrencies] = useState([]);
+
+  const fetchAllCurrencies = async () => {
+    await axios
+      .get("https://api.frankfurter.app/currencies")
+      .then((response) => {
+        const data = Object.entries(response.data).map(([code, name]) => ({
+          code,
+          name,
+        }));
+        setCurrencies(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error("Unexpected Internal Server Error!");
+      });
+  };
+
+  useEffect(() => {
+    fetchAllCurrencies();
+  }, []);
+
   return (
     <Card className="w-full mt-3 xl:mt-8">
       <CardHeader className="space-y-5 flex flex-col">
         <div className="w-full flex flex-col space-y-2">
-          <CurrencyDropdown title="From" />
-          <CurrencyDropdown title="To" />
+          <CurrencyDropdown title="From" currencies={currencies} />
+          <CurrencyDropdown title="To" currencies={currencies} />
         </div>
 
         <Input type="number" label="Amount" placeholder="0.00" isRequired />
--- src/components/CurrencyDropdown.jsx
+++ src/components/CurrencyDropdown.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 
-const CurrencyDropdown = ({ title }) => {
+const CurrencyDropdown = ({ title, currencies }) => {
   return (
     <>
       <label htmlFor={title}>{title}</label>
       <select className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-primary">
         <option value="">Select a currency</option>
-        {/* Currencies */}
+        {currencies.map((currency) => (
+          <option key={currency.code} value={currency.code}>
+            {currency.code} - {currency.name}
+          </option>
+        ))}
       </select>
     </>
   );
