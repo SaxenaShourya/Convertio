a54881db2d4510082e7458b0fea971de8448b0db
Shourya Saxena <saxenashourya000@gmail.com>
2024-04-23 19:26:34 +0530

    feat(style): Enhance responsiveness of ToastContainer for mobile devices

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,13 @@ const App = () => {
   return (
     <>
       <ToastContainer
-        position="bottom-right"
+        position="top-center"
         autoClose={3500}
         draggable
         pauseOnHover={false}
         theme="dark"
         transition={Slide}
-        toastClassName="font-outfit"
+        toastClassName="max-[480px]:w-[250px] sm:w-[22rem] rounded-lg text-sm sm:text-base font-outfit max-[480px]:top-0 max-[480px]:left-[calc(50%-125px)]"
       />
       <div className="h-full w-full px-8 lg:px-20 bg-gradient-to-tr from-green-200 to-white">
         <NavBar />
