18a016e845b2f2867aeb3d9e702f4e21ffc3acfb
Shourya Saxena <saxenashourya000@gmail.com>
2024-04-23 18:19:49 +0530

    feat(ui): Integrate ToastContainer from React Toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import React from "react";
+import { ToastContainer, Slide } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import { NavBar } from "./components";
 import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
-    <div className="h-full w-full px-8 lg:px-20 bg-gradient-to-tr from-green-200 to-white">
-      <NavBar />
-      <HomePage />
-    </div>
+    <>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={3500}
+        draggable
+        pauseOnHover={false}
+        theme="dark"
+        transition={Slide}
+        toastClassName="font-outfit"
+      />
+      <div className="h-full w-full px-8 lg:px-20 bg-gradient-to-tr from-green-200 to-white">
+        <NavBar />
+        <HomePage />
+      </div>
+    </>
   );
 };
 
